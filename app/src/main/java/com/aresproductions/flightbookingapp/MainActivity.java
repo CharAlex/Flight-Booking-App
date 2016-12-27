7526803ae95fc922cc88482b52fc67663afbc3d9
CharAlex <alexandros8a8@gmail.com>
2016-12-27 20:31:39 +0200

    UI (First Activity)

--- app/src/main/java/com/aresproductions/flightbookingapp/MainActivity.java
+++ app/src/main/java/com/aresproductions/flightbookingapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.aresproductions.flightbookingapp;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
